e587625a7f3e7df790f9ad5ffa6b52ba906ea4db
Eric Groom <egroom@mail.sfsu.edu>
2019-09-23 22:37:14 -0700

    set up time loop

--- static/time.js
+++ static/time.js
@@ -1,18 +1,45 @@
 const root = document.getElementById("root");
 const timeInput = document.getElementById("time-input");
 
-const timeString = (function() {
-  const timestamp = Date.parse(timeInput.value);
-  const date = new Date(timestamp);
-  const str = `${date.getHours()}:${date
-    .getMinutes()
-    .toString()
-    .padStart(2, "0")}`;
-  return str;
-})();
+const timestamp = Date.parse(timeInput.value);
+const dateFromServer = new Date(timestamp);
+const timeString = `${dateFromServer.getHours()}:${dateFromServer
+  .getMinutes()
+  .toString()
+  .padStart(2, "0")}`;
 
 const timeDisplay = document.createElement("p");
 timeDisplay.textContent = timeString;
 timeDisplay.classList.add("time");
 
 root.appendChild(timeDisplay);
+listenForTime();
+var happened = false;
+function listenForTime() {
+  const now = new Date();
+  console.log("bloop");
+  if (happened) {
+    happened = false;
+    const el = document.createElement("p");
+    el.textContent = "no mas";
+    root.appendChild(el);
+  }
+  if (hoursAndMinutesEqual(now, dateFromServer)) {
+    const el = document.createElement("p");
+    el.textContent = "it's happening!!!";
+    root.appendChild(el);
+    happened = true;
+  }
+  setTimeout(listenForTime, calculateUpdateInterval(now));
+}
+
+function calculateUpdateInterval(date) {
+  const secondsDelta = 59 - date.getSeconds();
+  const next = secondsDelta > 0 ? secondsDelta + 1 : 60;
+  console.log(`seconds: ${date.getSeconds()}, secondsDelta: ${secondsDelta}`);
+  return next * 1000;
+}
+
+function hoursAndMinutesEqual(a, b) {
+  return a.getHours() === b.getHours() && a.getMinutes() === b.getMinutes();
+}
