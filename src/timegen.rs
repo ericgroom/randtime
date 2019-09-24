738e2d0fa0576ba289ed59774a7f7386ce0b7410
Eric Groom <egroom@mail.sfsu.edu>
2019-09-24 08:27:26 -0700

    clean up that nested match

--- src/timegen.rs
+++ src/timegen.rs
@@ -4,6 +4,7 @@ extern crate rand;
 use chrono::prelude::*;
 use rand::prelude::*;
 use std::env;
+use std::fmt::Debug;
 use std::fs;
 use std::fs::File;
 use std::io::prelude::*;
@@ -24,27 +25,35 @@ fn parse_time(time: &str) -> Result<DateTime<Utc>, chrono::ParseError> {
     time.parse::<DateTime<Utc>>()
 }
 
-pub fn get_random_time() -> DateTime<Utc> {
-    let filepath = env::var("TIME_FILE");
-    let file = filepath.as_ref().map(Path::new).map(File::open);
+fn log_and_return(e: &dyn Debug) -> DateTime<Utc> {
+    println!("{:?}", e);
+    make_random_time()
+}
 
-    // bleh, cannot find a way to flatten
+pub fn get_random_time() -> DateTime<Utc> {
+    let filepath = match env::var("TIME_FILE") {
+        Ok(path) => path,
+        Err(e) => return log_and_return(&e),
+    };
+    let file = File::open(Path::new(&filepath));
     match file {
-        Ok(f) => match f {
-            Ok(f) => {
-                let mut reader = BufReader::new(f);
-                let mut contents = String::new();
-                let _ = reader.read_to_string(&mut contents);
-                parse_time(&contents).unwrap_or(make_random_time())
-            }
-            Err(_) => make_random_time(),
-        },
-        Err(_) => make_random_time(),
+        Ok(f) => read_time(f),
+        Err(e) => return log_and_return(&e),
+    }
+}
+
+fn read_time(file: File) -> DateTime<Utc> {
+    let mut reader = BufReader::new(file);
+    let mut contents = String::new();
+    let _ = reader.read_to_string(&mut contents);
+    match parse_time(&contents) {
+        Ok(time) => time,
+        Err(e) => log_and_return(&e),
     }
 }
 
 pub fn write_random_time() -> std::io::Result<()> {
-    let filepath = env::var("TIME_FILE").unwrap();
+    let filepath = env::var("TIME_FILE").expect("Cannot find environment variable TIME_FILE");
     let path = Path::new(&filepath);
     let time = make_random_time();
     let time_str = format!("{:?}", time);
